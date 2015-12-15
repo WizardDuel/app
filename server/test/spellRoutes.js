d36bec8eb8f817a61158d4a5f2cf7038a75209b5
Peter Madsen <cephas.ryle@gmail.com>
2015-12-15 10:10:21 -0800

    create before and after for spell test

--- server/test/spellRoutes.js
+++ server/test/spellRoutes.js
@@ -1,9 +1,18 @@
 var chai = require('chai');
-var expect = chai.expect;
+var mongoose = require('mongoose');
 
+var expect = chai.expect;
+var server;
 
 describe('Test Spell Lib Routes', function() {
-  before('Connect ')
+  before('Connect to server and Populate Data', function(done){
+     server = require('../server.js');
+     mongoose.
+  });
 
+  it('should get all spells from the database');
 
+  after('Close Connection and remove fake Data', function(done) {
+    done();
+  });
 });
