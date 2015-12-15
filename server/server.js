a860254b891b216ee6d8ab6ff23c0165646f6311
Peter Madsen <cephas.ryle@gmail.com>
2015-12-15 10:01:56 -0800

    create inital structure for spell routing

--- server/server.js
+++ server/server.js
@@ -6,11 +6,12 @@ require('./sockets')(io);
 
 //setup
 var mongoose = require('mongoose');
+mongoose.connect('mongodb//localhost:3000');
+
 app.use(express.static(__dirname + '/public'));
 
 //routes
 var spellRoutes = require('./routes/spellRoutes.js');
-
 app.use('/spells', spellRoutes);
 
 http.listen(3000, function() {
---
+++ server/test/spellRoutes.js
@@ -0,0 +1,9 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+
+describe('Test Spell Lib Routes', function() {
+  before('Connect ')
+
+
+});
