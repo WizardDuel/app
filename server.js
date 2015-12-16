2360d8e64dfecfd137b43cb33ee7c4735a5a8a95
Peter Madsen <cephas.ryle@gmail.com>
2015-12-15 23:15:40 -0800

    move public folder to client fix routing for landing page

--- server.js
+++ server.js
@@ -2,20 +2,25 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-require('./sockets')(io);
+require('./server-files/sockets')(io);
 
 var port = process.env.PORT || 3000;
 app.set('env', process.env.NODE_ENV);
+
+var path = require('path');
+app.get('/landing', function(req, res) {
+  res.sendFile(path.resolve(__dirname, './public/landing/landing.html'));
+});
 //setup
 // var mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost:3000');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/client'));
 
 //routes
-var spellRoutes = require('./routes/spellRoutes.js');
+var spellRoutes = require('./server-files/routes/spellRoutes.js');
 app.use('/spells', spellRoutes);
 
 http.listen(port, function() {
-  console.log('listening on *: ', port);
+  console.log('listening on: ', port);
 });
